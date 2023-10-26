8763b5724781c1faacf5efd9138a305e6b7056e4
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-26 22:59:14 +0900

    URLから#を消す

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:go_router_sample/ui/router/router_provider.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
+void main() {
+  usePathUrlStrategy();
+  runApp(const ProviderScope(child: App()));
+}
 
 class App extends ConsumerWidget {
   const App({super.key});
